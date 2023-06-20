940e5d23e80280cc228c256f700be224ca26d4d8
vktornaj <victor2211812@gmail.com>
2023-06-20 16:57:59 +0000

    fixed deployment details

--- cloud-deploy.sh
+++ cloud-deploy.sh
@@ -2,21 +2,25 @@
 
 set -e
 
-remote_host="3.139.84.148"
+remote_host="18.224.68.2"
 remote_port=22
 remote_user="admin"
 local_path=$(pwd)/
 remote_path="/home/${remote_user}/todo_rust/"
 ssh_key="/home/vktornaj/aws/key_001.pem"
-tag='0.0.2'
+tag='0.0.9'
 
 
 # Upadte source code
+echo "Cleaning"
 ssh -i ${ssh_key} ${remote_user}@${remote_host} sudo rm -rf todo_rust/
+echo "Logging"
 ssh -i ${ssh_key} ${remote_user}@${remote_host} aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 569233066229.dkr.ecr.us-east-2.amazonaws.com
+echo "Sending data"
 rsync -avzr --exclude='.git/' --exclude='target/' --delete -e "ssh -p ${remote_port} -i ${ssh_key} -o StrictHostKeyChecking=no" ${local_path} ${remote_user}@${remote_host}:${remote_path}
 
 # Build, tag and push docker image to aws ecr
+echo "Pushing docker image"
 ssh -tt -i ${ssh_key} ${remote_user}@${remote_host} << EOF 
 sudo docker build -t todo-rust:${tag} ${remote_path}
 sudo docker tag todo-rust:${tag} 569233066229.dkr.ecr.us-east-2.amazonaws.com/todo-rust:${tag}
--- src/adapter/driven/persistence/pgsql/migrations/2023-03-13-000627_create_todo/down.sql
+++ src/adapter/driven/persistence/pgsql/migrations/2023-03-13-000627_create_todo/down.sql
@@ -1,8 +1,10 @@
 -- This file should undo anything in `up.sql`
+-- ALTER TABLE IF EXISTS tag_entry DROP COLUMN tag_entry_id;
 DROP FUNCTION find_tags_sql(INT);
-DROP FUNCTION find_user_by_username(text);
-DROP FUNCTION find_todo_sql(varchar, varchar, varchar, INT, varchar[]);
-DROP FUNCTION create_tag(character varying, character varying);
+DROP FUNCTION find_user_by_username(VARCHAR);
+DROP FUNCTION find_todo_sql(VARCHAR, VARCHAR, VARCHAR, INT, VARCHAR[]);
+DROP FUNCTION create_tag(CHARACTER varying, CHARACTER varying);
+DROP FUNCTION update_todo(INT, VARCHAR, VARCHAR, INT, TIMESTAMPTZ, TIMESTAMPTZ);
 DROP TABLE _status;
 DROP TABLE _todo_tag;
 DROP TABLE _tag;
